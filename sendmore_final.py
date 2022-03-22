25a70c2c63a172f8df61b4399484d5f4958b3579
RealRichi3 <Molunorichie@gmail.com>
2022-03-22 20:45:26 +0100

    Created sendmore_final.py file

---
+++ sendmore_final.py
@@ -0,0 +1,11 @@
+"""
+SEND + MORE == MONEY
+
+Assigns different values from range 0-9 to each variable in sendmory
+And checks if the equation is satisfied
+
+CODE
+Assign random number to each letter
+Check if the combination has been used before
+Input parameters to equation
+"""
